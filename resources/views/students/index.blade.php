b05c0950d53a11b1e70d30172e327b2418340990
kelvins <kelvinchemiati@gmail.com>
2024-03-10 15:54:22 +0300

    students interface update

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,5 +1,90 @@
-@extends('layout')
+@extends('layouts.app')
+
 @section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Students
+                <small></small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active"><a href="/students">Students</a></li>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="box">
+                        <div class="box-header">
+                            <a href="{{ url('/students/create') }}" class="btn btn-primary">Add New</a>
+                        </div>
+                        @include('messages')
+                        <!-- /.box-header -->
+                        <div class="box-body no-padding">
+                            <table class="table table-striped">
+                                <tr>
+                                    <th style="width: 10px">#</th>
+                                    <th>Name</th>
+                                    <th>Address</th>
+                                    <th>Mobile</th>
+                                    <th>Actions</th>
+
+                                </tr>
+                                @foreach ($students as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->name }}</td>
+                                        <td>{{ $item->address }}</td>
+                                        <td>{{ $item->mobile }}</td>
+
+                                        <td>
+                                            <form method="POST" action="/students/{{ $item->id }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <a href="/students/{{ $item->id }}" class="btn btn-info btn-sm"><i
+                                                        class="fa fa-eye" aria-hidden="true"></i> view
+                                                </a>
+                                                <a href="/students/{{ $item->id }}/edit"
+                                                    class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o"></i>
+                                                    Edit
+                                                </a>
+                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Student"
+                                                    onclick="return confirm(&quot;Confirm delete?&quot;)"><i
+                                                        class="fa fa-trash-o" aria-hidden="true"></i>
+                                                    Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+                            </table>
+                        </div>
+                        <div class="box-footer clearfix">
+                            <ul class="pagination pagination-sm no-margin pull-right">
+                                <li><a href="#">&laquo;</a></li>
+                                <li><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#">&raquo;</a></li>
+                            </ul>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
+{{-- @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -7,7 +92,7 @@
                     <div class="card-header">
                         <h2>Students</h2>
                     </div>
-                    @include('messages')
+
                     <div class="card-body">
                         <a href="{{ url('/students/create') }}" class="btn btn-success btn-sm" title="Add New Student">
                             <i class="fa fa-plus" aria-hidden="true"></i> Add New
@@ -66,4 +151,4 @@
             </div>
         </div>
     </div>
-@endsection
+@endsection --}}
